13eb38b14d69df5656a9721c6c06754e1e81a994
b3t0 <wb2k9@yahoo.com>
2022-06-03 23:30:29 -0700

    adding playRound and starting game function

--- rps.js
+++ rps.js
@@ -1,14 +1,51 @@
-const randomChoice = ['rock', 'paper', 'scissors'];
+const playerSelection = computerPlay();
+const computerSelection = computerPlay();
 
 function computerPlay () {
-    // Math.floor(Math.random() * array.length)
-    // Math.floor returns a whole number - Math.random() returns a float between 0 and 1
-    return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+
+    let arrayItems = ['rock', 'paper', 'scissors'];
+    let randomChoice = arrayItems[Math.floor(Math.random() * arrayItems.length)];
+    // return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+    return randomChoice;
 }
 
-document.querySelector('p').innerHTML = computerPlay();
+// rock beats scissors, scissors beat paper by cutting it, and paper beats rock by covering it.
+function playRound(playerSelection, computerSelection) {
 
-// Test area
-console.log(Math.random(randomChoice) * toString().length);
-console.log(toString(randomChoice).length);
+    let playerChoice = playerSelection;
+    let computerChoice = computerSelection;
 
+    if (playerChoice === 'rock' && computerChoice === 'paper') {
+        return 'Computer Wins!';
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return 'Player Wins!';
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return 'Player Wins!';
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        return 'Computer Wins!';
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return 'Player Wins!';
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        return 'Computer Wins!';
+    } else {
+        return 'It\'s a tie'
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    return '';
+}
+
+// Testing area
+
+document.getElementById('result').innerHTML = playRound(playerSelection, computerSelection);
+document.getElementById("computer").innerHTML = 'Computer = ' + computerSelection;
+document.getElementById('player').innerHTML = 'Player = ' + playerSelection;
+
+console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(game());
