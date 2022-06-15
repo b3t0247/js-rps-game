dab2269ad1953393482d40e882da1ff6d62cd568
b3t0 <wb2k9@yahoo.com>
2022-06-15 12:54:06 -0700

    .

--- rps.js
+++ rps.js
@@ -1,15 +1,20 @@
-// const playerSelection = playerChoice();
 const computerSelection = computerPlay();
-const playerSelection = '';
-
-// function playerChoice() {
-//     playerSelection = document.getElementById("choice").value;
-//     console.log(playerSelection);
-// }
+var playerSelection = playerChoice();
 
 function playerChoice() {
-    let choice = document.getElementById("playerChoice").value;
-    return choice;
+    document.getElementById('choices').addEventListener('click', function(evt) {
+        playerSelection = evt.target;
+        if (playerSelection.id === 'rock') {
+            document.getElementById('player').innerHTML = 'Player = ' + 'rock';
+        } else if (playerSelection.id === 'paper') {
+            document.getElementById('player').innerHTML = 'Player = ' + 'paper';
+        } else if (playerSelection.id === 'scissors') {
+            document.getElementById('player').innerHTML = 'Player = ' + 'scissors';
+        } 
+        playerSelection = playerSelection.value;
+        // console.log(playerSelection);
+    }, false);
+    return playerSelection;
 }
 
 function computerPlay () {
@@ -46,13 +51,15 @@ function game() {
         // Call playRound function, passing in newly returned values
         // from playRound and computerPlay functions
         const currentRound = playRound(playerSelection, computerSelection);
-        // Log result to console and HTML
-        console.log(currentRound);
-        console.log(playerSelection);
-        console.log(computerSelection);
+        // Log result to HTML element and console
+        // console.log(playerSelection); // "rock" | "paper" | "scissors"
+        // console.log(computerSelection); // "rock" | "paper" | "scissors"
         const result = document.getElementById("result").innerHTML = playRound(playerSelection, computerSelection);
         const computer = document.getElementById("computer").innerHTML = 'Computer = ' + computerSelection;
         const player = document.getElementById("player").innerHTML = 'Player = ' + playerSelection;
+        console.log(currentRound);  // "Computer Wins!" | "Player Wins!" | "It's a draw! "
+        console.log(player);
+        console.log(computer);
         return {
             result, computer, player
         };
@@ -63,7 +70,6 @@ let play = document.getElementById('play');
 
 play.addEventListener('click', function() {
     let startGame = game();
-    // let startGame = document.getElementById('score').innerHTML = game();
     return startGame;
 })
 
